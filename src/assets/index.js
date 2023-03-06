1284d1112e73a17a716efebfe409bc56cd3966dc
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-03-06 17:01:28 -0500

    update assets and the indexJS file assets

---
+++ src/assets/index.js
@@ -0,0 +1,57 @@
+import airbnb from "./airbnb.png";
+import binance from "./binance.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.png";
+import quotes from "./quotes.svg";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import people04 from "./people04.jpg";
+import site1 from "./site1.jpg";
+import site2 from "./site2.jpg";
+import office1 from "./office1.jpg";
+import office2 from "./office2.jpg";
+
+export {
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+  people04,
+  site1,
+  site2,
+  office1,
+  office2
+};
