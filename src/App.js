f966f87356caedd958c1e3bb60c7eb10274e5f8e
JayAtSeneca <jpchaudhari@myseneca.ca>
2023-03-06 17:01:53 -0500

    update App component include contact component

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import styles from "./styles";
+import { Contact, Footer, Navbar} from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Contact/>
+        <Footer/>
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
